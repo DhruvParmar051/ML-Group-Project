0c73a505c861f5c7a9551a3fadd8260b558566c5
DhruvParmar051 <dhruvparmar051@outlook.com>
2025-08-27 13:49:50 +0530

    data_transformation completed

--- src/Linear Regression/component/data_transformation.py
+++ src/Linear Regression/component/data_transformation.py
@@ -10,6 +10,7 @@ from sklearn.pipeline import Pipeline
 
 from utils.exception import CustomException
 from utils.logger import setup_logger
+from utils.save_object import save_object
 
 logger = setup_logger()
 
@@ -24,7 +25,7 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
         
-    def get_data_transformation_object(self, df:pd.DataFrame):
+    def get_data_transformation_pipeline(self, df:pd.DataFrame):
         """
         Create and returns a preprocessing pipeline that imputes missing values and scales numerical values
         """
@@ -42,4 +43,62 @@ class DataTransformation:
             return num_pipeline
 
         except Exception as e:
-            logger.info()
+            logger.info('Data Preprocessor Failed.. %s', str(e))
+            raise CustomException(e, sys) from e
+        
+
+    def initiate_data_transformation(self, train_path: str, test_path: str, target_column: str,
+                                     id_columns: list = ["Ticker", "Company"]):
+        """
+        Applied data transformation onto training and test dataset.
+        """
+        
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            
+            numeric_cols = train_df.select_dtypes(include=["float64", "int64"]).columns.tolist()
+        
+            
+            if target_column in numeric_cols:
+                numeric_cols.remove(target_column)
+
+            X_train = train_df[numeric_cols]
+            y_train = train_df[target_column]
+            ids_train = train_df[id_columns]
+            
+            X_test = test_df[numeric_cols]
+            y_test = test_df[target_column]
+            ids_test = test_df[id_columns]
+            Pipeline = self.get_data_transformation_pipeline()
+            
+            logger.info('Applying Pipeline')
+            
+            X_train_transformed = Pipeline.fit_transform(X_train)
+            X_test_transformed = Pipeline.fit_transform(X_test)
+            
+            X_train_transformed = pd.DataFrame(
+                X_train_transformed, columns=numeric_cols, index=train_df.index
+            )
+            X_test_transformed = pd.DataFrame(
+                X_test_transformed, columns=numeric_cols, index=test_df.index
+            )
+            
+            train_transformed = pd.concat([ids_train, X_train_transformed, y_train], axis=1)
+            test_transformed = pd.concat([ids_test, X_test_transformed, y_test], axis=1)
+            
+            logger.info("💾 Saving preprocessing pipeline object...")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=Pipeline
+            )
+            
+            return train_transformed, test_transformed, self.data_transformation_config.preproceesor_obj_file_path
+                  
+                  
+            
+        except Exception as e:
+            logger.error("Data Transformation Failed %s", str(e))
+            raise CustomException(e, sys) from e
+        
